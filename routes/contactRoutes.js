3bf1e7425cd652b0acf552f1af10eb5beacdb1fa
Umar Basha -Tech Monk <umarbasha007@gmail.com>
2023-05-12 03:46:11 +0530

    Controller based API code approach
    Controller based API code approach

--- routes/contactRoutes.js
+++ routes/contactRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const { getContacts , createContact, getContact, updateContact, deleteContact } = require("../controllers/contactController");
 
 router.route("/home").get((req, res) => {
     res.status(200).json({ message : "This is a home page."})
@@ -8,24 +8,14 @@ router.route("/home").get((req, res) => {
 
 // router as per project need
 
-router.route("/").get((req, res) => {
-    res.status(200).json({ message : "Get all contacts."})
-});
+router.route("/").get(getContacts);
 
-router.route("/").post((req, res) => {
-    res.status(200).json({ message : "Create contact"})
-});
+router.route("/").post(createContact);
 
-router.route("/:id").get((req, res) => {
-    res.status(200).json({ message : `Get contact for ${req.params.id}`})
-});
+router.route("/:id").get(getContact);
 
-router.route("/:id").put((req, res) => {
-    res.status(200).json({ message : `Update contact for ${req.params.id}`})
-});
+router.route("/:id").put(updateContact);
 
-router.route("/:id").delete((req, res) => {
-    res.status(200).json({ message : `Delete contact for ${req.params.id}`})
-});
+router.route("/:id").delete(deleteContact);
 
 module.exports = router;
